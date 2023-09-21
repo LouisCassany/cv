b586a9443749d528c50ad27363492a712e995d03
Cuke7 <cuke.dev@mail.com>
2023-09-21 19:34:03 +0200

    tset

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cv = JSON.parse(fs.readFileSync("./cv.json", "utf-8"));
 
 let education = add_section(cv[0], "fr");
 let experience = add_section(cv[1], "fr");
+let skills = add_section(cv[2], "fr");
+let publications = add_section(cv[3], "fr");
 
 const html = `
 <!DOCTYPE html>
@@ -21,7 +23,7 @@ const html = `
 
 <body>
   <div class= "flex justify-center items-center md:p-10 my-8">
-    <div class="flex space-x-4">
+    <div class="flex space-x-16">
       <img src="./photo_CV.png" class="h-36 rounded-lg shadow-xl" />
       <div class="flex flex-col justify-between items-end">
         <span class="text-4xl text-blue-900"> Louis CASSANY </span>
@@ -32,9 +34,11 @@ const html = `
       </div>
     </div>
   </div>
-  <div class="columns-2 px-8 md:w-3/4 mx-auto">
+  <div class="columns-2 px-8 md:w-3/4 mx-auto pb-8">
     ${education}
     ${experience}
+    ${skills}
+    ${publications}
   </div>
 </body>
 
@@ -44,13 +48,22 @@ await writeFileSync("./index.html", html);
 
 function add_section(section, lang) {
   let html = ``;
-  html += `<h1 class="text-2xl text-blue-900 mx-auto font-bold text-center">${section.title[lang]}</h1>`;
-  html += `<hr class="w-48 mx-auto h-[3px] my-2 bg-blue-900"/>`;
+  let flag = false;
+
   for (const item of section.items) {
-    html += `<div class="flex flex-col w-full mb-4 break-inside-avoid-column md:px-16 px-4">`;
-    html += `<div class="flex justify-between items-center"><h2 class="text-lg font-bold text-blue-900">${item.title[lang]}</h2> <span class="text-sm">${item.date[lang]}</span></div>`;
-    html += `<div class="font-bold">${item.location[lang]}</div>`;
-    html += `<div class="whitespace-pre-line">${item.subtitle[lang]}</div>`;
+    html += `<div class="flex flex-col w-full mb-4 break-inside-avoid-column md:px-16 px-4 item">`;
+    if (!flag) {
+      html += `<h1 class="text-2xl text-blue-900 mx-auto font-bold text-center">${section.title[lang]}</h1>`;
+      html += `<hr class="w-48 mx-auto h-[3px] bg-blue-900 mb-4"/>`;
+      flag = true;
+    }
+    if (item.date || item.title)
+      html += `<div class="flex justify-between items-center">
+                <h2 class="text-lg font-bold text-blue-900">${item.title[lang]}</h2>
+                ${item.date ? `<span class="text-sm">${item.date[lang]}</span>` : ""}
+              </div>`;
+    if (item.location) html += `<div class="font-bold">${item.location[lang]}</div>`;
+    if (item.subtitle) html += `<div class="whitespace-pre-line">${item.subtitle[lang]}</div>`;
     if (item.list) {
       html += `<ul class="marker:text-blue-900 list-inside list-disc">`;
       for (const listItem of item.list[lang]) {
